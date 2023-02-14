c2d67cebd2d558843fcdc3cb3ab503feb48610fd
sidit77 <sidit77@gmail.com>
2023-02-14 11:59:47 +0100

    made gui the main binary

--- src/main.rs
+++ src/main.rs
@@ -1,118 +1,184 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+mod renderer;
+mod devices;
 mod util;
 
-use std::thread;
-use std::time::Duration;
-use anyhow::{anyhow, ensure, Result};
-use hidapi::HidApi;
+use std::sync::Arc;
+use std::time::Instant;
+use egui::Visuals;
+use glow::Context;
 use log::LevelFilter;
-use crate::util::PeekExt;
+use tao::event::{Event, WindowEvent};
+use tao::event_loop::{ControlFlow, EventLoop, EventLoopWindowTarget};
+use tao::menu::{ContextMenu, MenuItemAttributes};
+use tao::platform::run_return::EventLoopExtRunReturn;
+use tao::system_tray::SystemTrayBuilder;
+use tao::window::Icon;
+use crate::renderer::{create_display, GlutinWindowContext};
+use crate::renderer::egui_glow_tao::EguiGlow;
 
-const STEELSERIES: u16 = 0x1038;
 
-const ARCTIS_NOVA_7 : u16 = 0x2202;
-const ARCTIS_NOVA_7X: u16 = 0x2206;
-const ARCTIS_NOVA_7P: u16 = 0x220a;
-
-const SUPPORTED_VENDORS: &[u16] = &[STEELSERIES];
-const SUPPORTED_PRODUCTS: &[u16] = &[ARCTIS_NOVA_7, ARCTIS_NOVA_7X, ARCTIS_NOVA_7P];
-const REQUIRED_INTERFACE: i32 = 3;
-
-const STATUS_BUF_SIZE: usize = 8;
-const READ_TIMEOUT: i32 = 500;
-
-const HEADSET_OFFLINE: u8 = 0x00;
-const HEADSET_CHARGING: u8 = 0x01;
-
-const BATTERY_MAX: u8 =  0x04;
-const BATTERY_MIN: u8 =  0x00;
-
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub enum BatteryStatus {
-    Charging,
-    Level(u8)
-}
-
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub struct ChatMix {
-    game: u8,
-    chat: u8
-}
-
-impl Default for ChatMix {
-    fn default() -> Self {
-        Self {
-            game: 100,
-            chat: 100,
-        }
-    }
-}
-
-#[derive(Default, Debug, Copy, Clone, Eq, PartialEq)]
-pub enum HeadsetStatus {
-    #[default]
-    Disconnected,
-    Connected {
-        battery: BatteryStatus,
-        chat_mix: ChatMix
-    }
-}
-
-impl From<[u8; STATUS_BUF_SIZE]> for HeadsetStatus {
-    fn from(report: [u8; STATUS_BUF_SIZE]) -> Self {
-        let chat_mix = ChatMix {
-            game: report[4],
-            chat: report[5],
-        };
-        match report[3] {
-            HEADSET_OFFLINE => HeadsetStatus::Disconnected,
-            HEADSET_CHARGING => HeadsetStatus::Connected {
-                battery: BatteryStatus::Charging,
-                chat_mix,
-            },
-            _ => HeadsetStatus::Connected {
-                battery: BatteryStatus::Level({
-                    let level = report[2].min(BATTERY_MAX).max(BATTERY_MIN);
-                    (level - BATTERY_MIN) * (100 / (BATTERY_MAX - BATTERY_MIN))
-                }),
-                chat_mix,
-            }
-        }
-    }
-}
-
-fn main() -> Result<()> {
+fn main() {
     env_logger::builder()
         .filter_level(LevelFilter::Trace)
         .format_timestamp(None)
         .parse_default_env()
         .init();
 
-    let api = HidApi::new()?;
-    let device = api
-        .device_list()
-        .filter(|info| SUPPORTED_VENDORS.contains(&info.vendor_id()))
-        .filter(|info| SUPPORTED_PRODUCTS.contains(&info.product_id()))
-        .filter(|info| info.interface_number() == REQUIRED_INTERFACE)
-        .inspect(|info | {
-            log::info!("Found {} {}", info.manufacturer_string().unwrap_or(""), info.product_string().unwrap_or(""));
-        })
-        .collect::<Vec<_>>()
-        .first()
-        .peek(|info| log::info!("Selected {} {}", info.manufacturer_string().unwrap_or(""), info.product_string().unwrap_or("")))
-        .ok_or_else(|| anyhow!("No supported device found!"))?
-        .open_device(&api)?;
+    let mut device = devices::find_device().unwrap();
+    device.poll().unwrap();
+    log::info!("{:?}", device.get_battery_status());
 
-    let mut previous_status = None;
+    let mut event_loop = EventLoop::new();
 
-    let mut buf = [0u8; STATUS_BUF_SIZE];
-    loop {
-        device.write(&[0x00, 0xb0])?;
-        ensure!(device.read_timeout(&mut buf, READ_TIMEOUT)? == STATUS_BUF_SIZE);
-        let current_status = HeadsetStatus::from(buf);
-        if previous_status.map(|s| s != current_status).unwrap_or(true) {
-            log::info!("{:#?}", current_status);
-            previous_status = Some(current_status);
+    let icon = Icon::from_rgba(vec![0xff; 32 * 32 * 4], 32, 32).unwrap();
+    let mut tray_menu = ContextMenu::new();
+    let open_item = tray_menu.add_item(MenuItemAttributes::new("Open"));
+    let quit_item = tray_menu.add_item(MenuItemAttributes::new("Quit"));
+    let _tray = SystemTrayBuilder::new(icon, Some(tray_menu))
+        .with_tooltip("Connected")
+        .build(&event_loop)
+        .expect("Can not build system tray");
+
+
+    let mut window: Option<EguiWindow> = Some(EguiWindow::new(&event_loop));
+
+
+    let mut name = String::from("Simon");
+    let mut age = 24;
+
+    let mut next_device_poll = Instant::now();
+    event_loop.run_return(move |event, event_loop, control_flow| {
+        if next_device_poll <= Instant::now() {
+            next_device_poll = Instant::now() + device.poll().unwrap();
         }
-        thread::sleep(Duration::from_millis(500));
+        let next_update = window
+            .as_ref()
+            .and_then(|w|w.next_repaint)
+            .map(|t| t.min(next_device_poll))
+            .unwrap_or(next_device_poll);
+        *control_flow = match next_update <= Instant::now() {
+            true => ControlFlow::Poll,
+            false => ControlFlow::WaitUntil(next_update)
+        };
+        if window.as_mut().map(|w| w.handle_events(&event, |egui_ctx| {
+            egui::CentralPanel::default().show(egui_ctx, |ui| {
+                ui.vertical_centered_justified(|ui|{
+                    ui.heading("My egui Application");
+                    ui.horizontal(|ui| {
+                        let name_label = ui.label("Your name: ");
+                        ui.text_edit_singleline(&mut name)
+                            .labelled_by(name_label.id);
+                    });
+                    ui.add(egui::Slider::new(&mut age, 0..=120).text("age"));
+                    if ui.button("Click each year").clicked() {
+                        age += 1;
+                    }
+                    ui.label(format!("Hello '{}', age {}", name, age));
+                    ui.label(format!("Connected '{:?}'", device.is_connected()));
+                    ui.label(format!("Battery '{:?}'", device.get_battery_status()));
+                    ui.label(format!("Chatmix '{:?}'", device.get_chat_mix()));
+                });
+                //ui.spinner();
+            });
+        })).unwrap_or(false) {
+            window.take();
+            *control_flow = ControlFlow::Exit;
+        }
+
+        match event {
+            Event::MenuEvent { menu_id, ..} => {
+                if menu_id == open_item.clone().id() {
+                    match &mut window {
+                        None => window = Some(EguiWindow::new(event_loop)),
+                        Some(window) => {
+                            window.gl_window.window().set_focus();
+                        }
+                    }
+                }
+                if menu_id == quit_item.clone().id() {
+                    *control_flow = ControlFlow::Exit;
+                }
+            }
+            _ => (),
+        }
+    });
+}
+
+struct EguiWindow {
+    gl_window: GlutinWindowContext,
+    gl: Arc<Context>,
+    egui_glow: EguiGlow,
+    next_repaint: Option<Instant>
+}
+
+impl EguiWindow {
+
+    fn new(event_loop: &EventLoopWindowTarget<()>) -> Self {
+        let (gl_window, gl) = create_display(&event_loop);
+        let gl = Arc::new(gl);
+        let egui_glow = EguiGlow::new(&event_loop, gl.clone(), None);
+        egui_glow.egui_ctx.set_visuals(Visuals::light());
+        gl_window.window().set_visible(true);
+
+        Self {
+            gl_window,
+            gl,
+            egui_glow,
+            next_repaint: Some(Instant::now()),
+        }
+    }
+
+    fn redraw(&mut self, gui: impl FnMut(&egui::Context)) {
+        let repaint_after = self.egui_glow.run(self.gl_window.window(), gui);
+        self.next_repaint = Instant::now().checked_add(repaint_after);
+        {
+            let clear_color = [0.1, 0.1, 0.1];
+            unsafe {
+                use glow::HasContext as _;
+                self.gl.clear_color(clear_color[0], clear_color[1], clear_color[2], 1.0);
+                self.gl.clear(glow::COLOR_BUFFER_BIT);
+            }
+            self.egui_glow.paint(self.gl_window.window());
+            self.gl_window.swap_buffers().unwrap();
+        }
+    }
+
+    fn handle_events(&mut self, event: &Event<()>, gui: impl FnMut(&egui::Context)) -> bool{
+        if self.next_repaint.map(|t| Instant::now().checked_duration_since(t)).is_some() {
+            self.gl_window.window().request_redraw();
+        }
+        match event {
+            Event::RedrawEventsCleared if cfg!(windows) => self.redraw(gui),
+            Event::RedrawRequested(_) if !cfg!(windows) => self.redraw(gui),
+            Event::WindowEvent { event, .. } => {
+                match &event {
+                    WindowEvent::CloseRequested | WindowEvent::Destroyed => {
+                        return true;
+                    },
+                    WindowEvent::Resized(physical_size) => {
+                        self.gl_window.resize(*physical_size);
+                    }
+                    WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                        self.gl_window.resize(**new_inner_size);
+                    },
+                    _ => {}
+                }
+
+                let event_response = self.egui_glow.on_event(&event);
+                if event_response.repaint {
+                    self.gl_window.window().request_redraw();
+                }
+            }
+            _ => (),
+        }
+        false
+    }
+}
+
+impl Drop for EguiWindow {
+    fn drop(&mut self) {
+        self.egui_glow.destroy();
     }
 }
