7bc857dac56f8edfcc9331a02f9cb62c8fbbf244
sidit77 <sidit77@gmail.com>
2023-03-02 21:41:19 +0100

    updated the audio action selector

--- src/ui/central_panel/headset.rs
+++ src/ui/central_panel/headset.rs
@@ -57,47 +57,67 @@ pub fn headset_section(ui: &mut Ui, debouncer: &mut Debouncer, auto_update: bool
     ui.add_space(10.0);
 }
 
+fn get_name(switch: &OsAudio) -> &str {
+    match switch {
+        OsAudio::Disabled => "Disabled",
+        OsAudio::ChangeDefault { .. } => "Change Default Device",
+        OsAudio::RouteAudio { .. } => "Route Audio When Disconnected"
+    }
+}
+
 fn audio_output_switch_selector(ui: &mut Ui, switch: &mut OsAudio, audio_system: &mut AudioSystem) -> bool {
     let mut dirty = false;
-    let mut enabled = *switch != OsAudio::Disabled;
-    if ui.checkbox(&mut enabled, "Automatic Output Switching").changed() {
-        if enabled {
-            audio_system.refresh_devices();
-            let default_audio = audio_system
+    let resp = ComboBox::from_label("Audio Action")
+        .selected_text(get_name(switch))
+        .width(250.0)
+        .show_ui(ui, |ui| {
+            let default_device = audio_system
                 .default_device()
                 .or_else(||audio_system
                     .devices()
                     .first())
                 .map(|d| d.name().to_string())
-                .expect("No device");
-            *switch = OsAudio::ChangeDefault {
-                on_connect: default_audio.clone(),
-                on_disconnect: default_audio,
-            };
-        } else {
-            *switch = OsAudio::Disabled;
-        }
-        dirty |= true;
+                .unwrap_or_else(|| String::from("<None>"));
+            let options = [
+                OsAudio::Disabled,
+                OsAudio::ChangeDefault { on_connect: default_device.clone(), on_disconnect: default_device.clone() },
+                OsAudio::RouteAudio { src: default_device.clone(), dst: default_device.clone() }
+            ];
+            for option in options {
+                let current = std::mem::discriminant(switch) == std::mem::discriminant(&option);
+                if ui.selectable_label(current, get_name(&option)).clicked() && !current {
+                    *switch = option;
+                    dirty = true;
+                }
+            }
+        });
+    if resp.response.clicked() {
+        audio_system.refresh_devices();
     }
     if let OsAudio::ChangeDefault { on_connect, on_disconnect } = switch {
         dirty |= audio_device_selector(ui, "On Connect", on_connect, audio_system.devices());
         dirty |= audio_device_selector(ui, "On Disconnect", on_disconnect, audio_system.devices());
     }
+    if let OsAudio::RouteAudio { src, dst } = switch {
+        dirty |= audio_device_selector(ui, "From", src, audio_system.devices());
+        dirty |= audio_device_selector(ui, "To", dst, audio_system.devices());
+    }
     dirty
 }
 
 fn audio_device_selector(ui: &mut Ui, label: &str, selected: &mut String, audio_devices: &[AudioDevice]) -> bool {
-    let (mut index, mut changed) = audio_devices
-        .iter()
-        .position(|d| d.name() == selected)
-        .map(|i| (i, false))
-        .unwrap_or((0, true));
-    changed |= ComboBox::from_label(label)
+    let mut changed = false;
+    ComboBox::from_label(label)
         .width(300.0)
-        .show_index(ui, &mut index, audio_devices.len(), |i| audio_devices[i].name().to_string())
-        .changed();
-    if changed {
-        *selected = audio_devices[index].name().to_string();
-    }
+        .selected_text(selected.as_str())
+        .show_ui(ui, |ui|{
+            for dev in audio_devices {
+                let current = dev.name() == selected;
+                if ui.selectable_label(current, dev.name()).clicked() && !current {
+                    *selected = dev.name().to_string();
+                    changed = true;
+                }
+            }
+        });
     changed
 }
