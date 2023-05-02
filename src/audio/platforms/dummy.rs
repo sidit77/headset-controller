8800a0ce0e4c8016fa5ae5a594e51501d0f5c9c4
sidit77 <sidit77@gmail.com>
2023-05-02 21:05:56 +0200

    dummy audio platform

---
+++ src/audio/platforms/dummy.rs
@@ -0,0 +1,42 @@
+use anyhow::{bail, Result};
+
+#[derive(Debug, Clone)]
+pub struct AudioManager;
+impl AudioManager {
+
+    pub fn new() -> anyhow::Result<Self> {
+        bail!("Not supported on this platform!")
+    }
+
+    pub fn devices(&self) -> impl Iterator<Item=AudioDevice> {
+        std::iter::empty::<AudioDevice>()
+    }
+
+    pub fn get_default_device(&self) -> Option<AudioDevice> {
+        None
+    }
+
+    pub fn set_default_device(&self, _: &AudioDevice) -> Result<()> {
+        bail!("not supported!");
+    }
+
+
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct AudioDevice;
+impl AudioDevice {
+
+    pub fn name(&self) -> &str {
+        unimplemented!()
+    }
+
+}
+
+pub struct AudioLoopback;
+
+impl AudioLoopback {
+    pub fn new(_: &AudioDevice, _: &AudioDevice) -> Result<Self> {
+        bail!("not supported!")
+    }
+}
