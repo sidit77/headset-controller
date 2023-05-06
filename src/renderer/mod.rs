848300bf3b5f977f28b6c17c24ba0620d89def07
sidit77 <sidit77@gmail.com>
2023-05-06 23:21:27 +0200

    fixed the opengl error on linux

--- src/renderer/mod.rs
+++ src/renderer/mod.rs
@@ -1,12 +1,13 @@
 pub mod egui_glow_tao;
 
+use std::num::NonZeroU32;
 use egui::NumExt;
 use glutin::config::ConfigTemplateBuilder;
-use glutin::context::{ContextAttributesBuilder, PossiblyCurrentContext};
+use glutin::context::{ContextApi, ContextAttributesBuilder, PossiblyCurrentContext};
 use glutin::display::{Display, GetGlDisplay};
 use glutin::prelude::*;
-use glutin::surface::{Surface, WindowSurface};
-use glutin_tao::{finalize_window, ApiPrefence, DisplayBuilder};
+use glutin::surface::{Surface, SwapInterval, WindowSurface};
+use glutin_tao::{finalize_window, DisplayBuilder, ApiPreference, GlWindow};
 use raw_window_handle::HasRawWindowHandle;
 use tao::dpi::{LogicalSize, PhysicalSize};
 use tao::event_loop::EventLoopWindowTarget;
@@ -25,78 +26,77 @@ impl GlutinWindowContext {
     // preferably add android support at the same time.
     #[allow(unsafe_code)]
     unsafe fn new(event_loop: &EventLoopWindowTarget<()>) -> Self {
-        let winit_window_builder = WindowBuilder::new()
+        let window_builder = WindowBuilder::new()
             .with_resizable(true)
             .with_inner_size(LogicalSize { width: 800.0, height: 600.0 })
             .with_window_icon(Some(crate::ui::WINDOW_ICON.clone()))
-            .with_title("Headset Controller") // Keep hidden until we've painted something. See https://github.com/emilk/egui/pull/2279
-            .with_visible(false);
+            .with_title("Headset Controller");
 
         #[cfg(windows)]
         let winit_window_builder = tao::platform::windows::WindowBuilderExtWindows::with_drag_and_drop(winit_window_builder, false);
 
-        let config_template_builder = ConfigTemplateBuilder::new()
-            .prefer_hardware_accelerated(None)
+        let template = ConfigTemplateBuilder::new()
             .with_depth_size(0)
             .with_stencil_size(0)
-            .with_transparency(false);
+            .with_transparency(false)
+            .prefer_hardware_accelerated(None);
 
-        tracing::debug!("trying to get gl_config");
-        let (mut window, gl_config) = DisplayBuilder::new() // let glutin-winit helper crate handle the complex parts of opengl context creation
-            .with_preference(ApiPrefence::FallbackEgl) // https://github.com/emilk/egui/issues/2520#issuecomment-1367841150
-            .with_window_builder(Some(winit_window_builder.clone()))
-            .build(event_loop, config_template_builder, |mut config_iterator| {
-                config_iterator
-                    .next()
-                    .expect("failed to find a matching configuration for creating glutin config")
+        let display_builder = DisplayBuilder::new()
+            .with_preference(ApiPreference::FallbackEgl)
+            .with_window_builder(Some(window_builder.clone()));
+
+        let (mut window, gl_config) = display_builder
+            .build(&event_loop, template, |configs| {
+                configs.reduce(|accum, config| match config.num_samples() > accum.num_samples() {
+                    true => config,
+                    false => accum
+                }).expect("failed to find a matching configuration for creating glutin config")
             })
             .expect("failed to create gl_config");
+
+        println!("Picked a config with {} samples", gl_config.num_samples());
+
+        let raw_window_handle = window.as_ref().map(|window| window.raw_window_handle());
         let gl_display = gl_config.display();
-        tracing::debug!("found gl_config: {:?}", &gl_config);
 
-        let raw_window_handle = window.as_ref().map(|w| w.raw_window_handle());
-        tracing::debug!("raw window handle: {:?}", raw_window_handle);
-        let context_attributes = ContextAttributesBuilder::new().build(raw_window_handle);
-        // by default, glutin will try to create a core opengl context. but, if it is not available, try to create a gl-es context using this fallback attributes
-        let fallback_context_attributes = ContextAttributesBuilder::new()
-            .with_context_api(glutin::context::ContextApi::Gles(None))
+        let context_attributes = ContextAttributesBuilder::new()
             .build(raw_window_handle);
-        let not_current_gl_context = unsafe {
-            gl_display
-                .create_context(&gl_config, &context_attributes)
-                .unwrap_or_else(|_| {
-                    tracing::debug!(
-                        "failed to create gl_context with attributes: {:?}. retrying with fallback context attributes: {:?}",
-                        &context_attributes,
-                        &fallback_context_attributes
-                    );
-                    gl_config
-                        .display()
-                        .create_context(&gl_config, &fallback_context_attributes)
-                        .expect("failed to create context even with fallback attributes")
-                })
-        };
 
-        // this is where the window is created, if it has not been created while searching for suitable gl_config
-        let window = window.take().unwrap_or_else(|| {
-            tracing::debug!("window doesn't exist yet. creating one now with finalize_window");
-            finalize_window(event_loop, winit_window_builder.clone(), &gl_config).expect("failed to finalize glutin window")
+        let fallback_context_attributes = ContextAttributesBuilder::new()
+            .with_context_api(ContextApi::Gles(None))
+            .build(raw_window_handle);
+
+        let mut not_current_gl_context = Some(unsafe {
+            gl_display.create_context(&gl_config, &context_attributes).unwrap_or_else(|_| {
+                gl_display.create_context(&gl_config, &fallback_context_attributes).expect("failed to create context")
+            })
         });
-        let (width, height): (u32, u32) = window.inner_size().into();
-        let width = std::num::NonZeroU32::new(width.at_least(1)).unwrap();
-        let height = std::num::NonZeroU32::new(u32::at_least(height, 1)).unwrap();
-        let surface_attributes = glutin::surface::SurfaceAttributesBuilder::<WindowSurface>::new().build(window.raw_window_handle(), width, height);
-        tracing::debug!("creating surface with attributes: {:?}", &surface_attributes);
+
+        let window = window.take().unwrap_or_else(|| {
+            println!("window doesn't exist yet. creating one now with finalize_window");
+            finalize_window(&event_loop, window_builder, &gl_config)
+                .expect("failed to finalize glutin window")
+        });
+
+        let attrs = window
+            .build_surface_attributes(<_>::default());
+        println!("creating surface with attributes: {:?}", &attrs);
         let gl_surface = unsafe {
-            gl_display
-                .create_window_surface(&gl_config, &surface_attributes)
+            gl_config
+                .display()
+                .create_window_surface(&gl_config, &attrs)
                 .unwrap()
         };
-        tracing::debug!("surface created successfully: {gl_surface:?}.making context current");
-        let gl_context = not_current_gl_context.make_current(&gl_surface).unwrap();
+        println!("surface created successfully: {gl_surface:?}.making context current");
+        let gl_context = not_current_gl_context
+            .take()
+            .unwrap()
+            .make_current(&gl_surface)
+            .unwrap();
+
 
         gl_surface
-            .set_swap_interval(&gl_context, glutin::surface::SwapInterval::Wait(std::num::NonZeroU32::new(1).unwrap()))
+            .set_swap_interval(&gl_context, SwapInterval::Wait(NonZeroU32::new(1).unwrap()))
             .unwrap();
 
         GlutinWindowContext {
